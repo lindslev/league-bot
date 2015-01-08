639cb22451952a526f5b85c2bc5cfb38f4cd3676
Lindsay <ilinds4@aol.com>
2015-01-08 13:08:35 -0500

    lighter background tiles, maps on schedule page

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -5,6 +5,10 @@ angular.module('mltpApp')
 
     $scope.weekarr = ['1/18/2015', '1/25/2015','2/1/2015','2/8/2015','2/15/2015','2/22/2015',
                       '3/1/2015','3/8/2015','3/15/2015']
+    $scope.maparr = [['http://i.imgur.com/CDcTbs0.png#map-smirk','Smirk'],['http://i.imgur.com/wLQsUUc.png#map-velocity','Velocity'],
+                    ['http://i.imgur.com/iEmJy16.png#map-wormy','Wormy'],['http://i.imgur.com/9uZSGOn.png#map-danger-zone','Danger Zone 3'],
+                    ['http://i.imgur.com/zfqqa5C.png','Iron'],['http://i.imgur.com/G2IRxWd.png#map-45','45'],
+                    ['http://i.imgur.com/VixChXZ.png#map-boombox','Boombox'],['http://i.imgur.com/xgoaXJy.png#map-star','Star'],['/#/schedule','Community Vote'],['http://i.imgur.com/CDcTbs0.png#map-smirk','Smirk']];
 
     var teamsdata, scheddata;
     $http.post('/api/teams', {})
